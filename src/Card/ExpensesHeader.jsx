fc8217ee0ab943d46b1b390ac188fe4726061dbd
kibirige <kibirigejohn005@gmail.com>
2023-09-03 22:32:41 +0300

    implement the delete button

--- src/Card/ExpensesHeader.jsx
+++ src/Card/ExpensesHeader.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 const ExpensesHeader = () => {
   return (
-    <tr className="border-b border-pink-700 w-[22.5%] text-center text-pink-800 font-semibold">
-      <th className=" font-semi-bold ">Shopping</th>
-      <th className="  border-l border-r border-pink-700 w-[22.5%] text-center text-pink-800 font-semibold">
+    <tr className="border-b border-pink-700 w-[22.5%] text-center text-slate-500 font-semibold">
+      <th className=" font-normal  ">Shopping</th>
+      <th className="  border-l border-r border-pink-700 w-[22.5%] text-center text-slate-500 font-normal ">
         Wages
       </th>
-      <th className=" border-r border-pink-700 w-[22.5%] text-center text-pink-800 font-semibold">
+      <th className=" border-r border-pink-700 w-[22.5%] text-center text-slate-500 font-normal ">
         Utilities
       </th>
-      <th className="  border-r border-pink-700 w-[22.5%] text-center text-pink-800 font-semibold">
+      <th className="  border-r border-pink-700 w-[22.5%] text-center text-slate-500 font-normal ">
         Gross
       </th>
       <th className="w-[10%]"> </th>
--- src/Card/ExpensesRow.jsx
+++ src/Card/ExpensesRow.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import DeleteIcon from '../svgs/Delete';
+import { ref, remove, database } from '../../firebasae';
+import { getTodayDate } from '../javascript/date';
 
 const ExpensesRow = ({ shopping, wages, utilities, gross, id }) => {
+  const handleDelete = () => {
+    const path = `balance-beam/${getTodayDate()}/${id}`;
+    const itemLocation = ref(database, path);
+    remove(itemLocation);
+  };
+
   return (
     <tr className=" border-b border-pink-700 text-pink-700">
       <td className="px-1">{shopping}</td>
@@ -9,7 +17,9 @@ const ExpensesRow = ({ shopping, wages, utilities, gross, id }) => {
       <td className="border-r border-l border-pink-700 px-1">{utilities}</td>
       <td className="border-r border-l border-pink-700 px-1">{gross}</td>
       <td className=" flex justify-center items-center">
-        <DeleteIcon />
+        <button className="del" onClick={handleDelete}>
+          <DeleteIcon />
+        </button>
       </td>
     </tr>
   );
